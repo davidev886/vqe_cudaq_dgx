2c1776747e681c20b34a5d5e87a970738d7044c9
Davide Vodola <davide.vodola@basf.com>
2023-11-27 09:53:48 +0100

    Add info on time

--- vqe_cas_layers.py
+++ vqe_cas_layers.py
@@ -6,6 +6,9 @@ import json
 from src.vqe_cudaq_qnp import VqeQnp
 from src.utils_cudaq import get_cudaq_hamiltonian
 import time
+from collections import defaultdict
+import pandas as pd
+
 
 if __name__ == "__main__":
     np.set_printoptions(precision=6, suppress=True, linewidth=10000)
@@ -36,9 +39,12 @@ if __name__ == "__main__":
     empty_orbitals = num_active_orbitals - ((num_active_electrons // 2) + (num_active_electrons % 2))
     init_mo_occ = [2] * (num_active_electrons // 2) + [1] * (num_active_electrons % 2) + [0] * empty_orbitals
     system_name = f"FeNTA_s_{spin}_{basis.lower()}_{num_active_electrons}e_{num_active_orbitals}o"
+    info_time = defaultdict(list)
     results = []
-    for n_vqe_layers in [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]:
+    for n_vqe_layers in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]:
+
         print("# Start VQE with init_mo_occ", init_mo_occ, "layers", n_vqe_layers)
+        time_start = time.time()
         vqe = VqeQnp(n_qubits=n_qubits,
                      n_layers=n_vqe_layers,
                      init_mo_occ=init_mo_occ,
@@ -60,6 +66,16 @@ if __name__ == "__main__":
                    f"cas_{num_active_electrons}e_{num_active_orbitals}o_"
                    f"layer_{n_vqe_layers}.dat",
                    exp_vals)
+        time_end = time.time()
+        info_time["num_layer"].append(n_vqe_layers)
+        info_time["time_vqe"].append(time_end - time_start)
+
+        if len(info_time["num_layer"])  > 1:
+            df = pd.DataFrame(info_time)
+            df.to_csv(f'{system_name}_info_time_layers.csv')
 
     np.savetxt(f"energy_fenta_{basis.lower()}_cas_{num_active_electrons}e_{num_active_orbitals}o.dat",
                np.array(results))
+
+    df = pd.DataFrame(info_time)
+    df.to_csv(f'{system_name}_info_time_layers.csv')
